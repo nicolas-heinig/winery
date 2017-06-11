4de5e51b9a6c5d1726f7eb119f0b2498eb9ac01e
Nicolas Heinig <NHeinig@gmx.net>
2017-06-11 18:58:23 +0200

    Make the default amount for orders 0; Open PDFs in new tab

--- db/migrate/20170606174546_create_orders.rb
+++ db/migrate/20170606174546_create_orders.rb
@@ -3,8 +3,8 @@ class CreateOrders < ActiveRecord::Migration[5.0]
     create_table :orders do |t|
       t.belongs_to :customer, foreign_key: true
       t.belongs_to :wine, foreign_key: true
-      t.integer :boxes
-      t.integer :bottles
+      t.integer :boxes, default: 0
+      t.integer :bottles, default: 0
       t.boolean :shipped
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(version: 20170606174546) do
   create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "customer_id"
     t.integer  "wine_id"
-    t.integer  "boxes"
-    t.integer  "bottles"
+    t.integer  "boxes",       default: 0
+    t.integer  "bottles",     default: 0
     t.boolean  "shipped"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id", using: :btree
     t.index ["wine_id"], name: "index_orders_on_wine_id", using: :btree
   end
