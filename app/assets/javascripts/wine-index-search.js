6c7974ddc64f35b41ea4a46a2a983bc408f0d491
Nicolas Heinig <NHeinig@gmx.net>
2017-12-08 16:11:06 +0100

    Add search to wines index

--- app/assets/javascripts/wine-index-search.js
+++ app/assets/javascripts/wine-index-search.js
@@ -1,47 +1,50 @@
 document.addEventListener("turbolinks:load", () => {
-  $('.js-customer-index-search').on("input", () => {
+  $('.js-wine-index-search').on("input", () => {
     $('tbody').empty()
 
-    let query = $('.js-customer-index-search').val()
+    let query = $('.js-wine-index-search').val()
     
     if (!(query == '')) {
       $.get({
-        url: '/search/customers?query=' + query
+        url: '/search/wines?query=' + query
       }).done((result) => {
-        result.forEach((customer) => {
-          $('tbody').append(result_field_for_customer(customer))
+        result.forEach((wine) => {
+          $('tbody').append(result_field_for_wine(wine))
         })
       })   
     } 
   })
 })
 
-const result_field_for_customer = (customer) => {
+const result_field_for_wine = (wine) => {
   return("<tr>" +
-    "<td>" + customer.id + "</td>" +
-    "<td>" + customer.first_name + "</td>" +
-    "<td>" + customer.last_name + "</td>" +
-    "<td>" + customer.address + "</td>" +
-    "<td>" + customer.phone + "</td>" +
-    links_for_customer(customer) +
+    "<td>" + wine.id + "</td>" +
+    "<td>" + wine.name + "</td>" +
+    "<td>" + wine.volume + "</td>" +
+    "<td>" + price(wine.bottle_price) + "</td>" +
+    "<td>" + price(wine.box_price) + "</td>" +
+    "<td>" + wine.year + "</td>" +
+    "<td>" + wine.from + "</td>" +
+    "<td>" + wine.wine_type + "</td>" +
+    links_for_wine(wine) +
   "</tr>")
 }
 
-const links_for_customer = (customer) => {
-  const id = customer.id
+const links_for_wine = (wine) => {
+  const id = wine.id
 
   return(`<td class="hidden-sm hidden-xs">
-      <a href="/customers/${id}">
+      <a href="/wines/${id}">
         <span class="glyphicon glyphicon-eye-open"></span> Anzeigen
       </a>
     </td>
     <td class="hidden-sm hidden-xs">
-      <a href="/customers/${id}/edit">
+      <a href="/wines/${id}/edit">
         <span class="glyphicon glyphicon-pencil"></span> Bearbeiten
       </a> 
     </td> 
     <td class="hidden-sm hidden-xs"> 
-      <a data-confirm="Bist du sicher?" rel="nofollow" data-method="delete" href="/customers/${id}">
+      <a data-confirm="Bist du sicher?" rel="nofollow" data-method="delete" href="/wines/${id}">
         <span class="glyphicon glyphicon-trash"></span> Löschen
       </a> 
     </td>
@@ -52,21 +55,25 @@ const links_for_customer = (customer) => {
         </a>
         <ul class="dropdown-menu dropdown-menu-right">
           <li> 
-            <a href="/customers/${id}">
+            <a href="/wines/${id}">
               <span class="glyphicon glyphicon-eye-open"></span> Anzeigen
             </a>
           </li> 
           <li>
-            <a href="/customers/${id}/edit">
+            <a href="/wines/${id}/edit">
               <span class="glyphicon glyphicon-pencil"></span> Bearbeiten
             </a>
           </li>
           <li>
-            <a data-confirm="Bist du sicher?" rel="nofollow" data-method="delete" href="/customers/${id}"> 
+            <a data-confirm="Bist du sicher?" rel="nofollow" data-method="delete" href="/wines/${id}"> 
               <span class="glyphicon glyphicon-trash"></span> Löschen
             </a>
           </li>
         </ul> 
       </div>
     </td>`)
+}
+
+const price = (number) => {
+  return(Math.round(number).toFixed(2).toString().replace('.', ',') + ' €')
 }
